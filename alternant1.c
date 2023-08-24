535ce986089ff97e4df2c891f12c57a85abb1f80
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-24 07:35:32 +0300

    Update alternant1.c

--- alternant1.c
+++ alternant1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main()
+{
+    char c, cifra_anterioara;
+    unsigned short nrcif = 0;
+
+    scanf("%c", &c);
+
+    while (c >= '0' && c <= '9') {
+        nrcif++;
+
+        if (cifra_anterioara % 2 == c % 2) {
+            printf("nu");
+            return 0;
+        }
+
+        cifra_anterioara = c;
+
+        scanf("%c", &c);
+    }
+    
+    nrcif > 0 ? printf("da") : printf("nu");
+
+    return 0;
+}
+// 70/100
